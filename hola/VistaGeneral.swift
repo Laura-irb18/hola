36982672674b76e5830fd889c604ed73675e3101
Laura Rojas Bustamante <151399958+Laura-irb18@users.noreply.github.com>
2024-10-16 17:36:53 +0200

    vista general ok

---
+++ hola/VistaGeneral.swift
@@ -0,0 +1,18 @@
+//
+//  VistaGeneral.swift
+//  hola
+//
+//  Created by Laura Isabel Rojas Bustamante on 16/10/24.
+//
+
+import SwiftUI
+
+struct VistaGeneral: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaGeneral()
+}
---
+++ hola/VistaGeneralDos.swift
@@ -0,0 +1,24 @@
+//
+//  VistaGeneralDos.swift
+//  hola
+//
+//  Created by Laura Isabel Rojas Bustamante on 16/10/24.
+//
+
+
+import SwiftUI
+
+struct VistaGeneralDos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+        Text("Holiii")
+  
+  
+    }
+}
+
+#Preview {
+    VistaGeneralDos()
+}

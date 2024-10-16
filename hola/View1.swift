93447d4716029edc4e1d617b19e0dad027143102
Laura Rojas Bustamante <151399958+Laura-irb18@users.noreply.github.com>
2024-10-16 17:26:06 +0200

    commit one

---
+++ hola/View1.swift
@@ -0,0 +1,22 @@
+//
+//  View1.swift
+//  hola
+//
+//  Created by Laura Isabel Rojas Bustamante on 16/10/24.
+//
+
+import SwiftUI
+
+struct View1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View1()
+}

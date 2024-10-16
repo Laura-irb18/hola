0b03959b6582c2fa9e58fe48611e2137769fc24a
Laura Rojas Bustamante <151399958+Laura-irb18@users.noreply.github.com>
2024-10-16 17:32:34 +0200

    vista detalle

---
+++ hola/vistaDetalle.swift
@@ -0,0 +1,19 @@
+//
+//  vistaDetalle.swift
+//  hola
+//
+//  Created by Laura Isabel Rojas Bustamante on 16/10/24.
+//
+
+import SwiftUI
+
+struct vistaDetalle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "house")
+    }
+}
+
+#Preview {
+    vistaDetalle()
+}

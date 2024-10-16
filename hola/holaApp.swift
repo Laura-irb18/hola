3563ac2bf384e9440d6fa4f9bd3b34e4f4ca10e8
Laura Rojas Bustamante <151399958+Laura-irb18@users.noreply.github.com>
2024-10-16 17:06:39 +0200

    Initial Commit

---
+++ hola/holaApp.swift
@@ -0,0 +1,17 @@
+//
+//  holaApp.swift
+//  hola
+//
+//  Created by Laura Isabel Rojas Bustamante on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct holaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
